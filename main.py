b893cc35b72f5b79f8a4e555135eeb26a6755917
1lubo <1lubo@pm.me>
2022-02-21 16:45:57 +0100

    add main.py

--- main.py
+++ main.py
@@ -1,2 +1,75 @@
+
+def get_num_teams():
+    while True:
+        num_teams = int(input("Enter the number of teams in the tournament: "))
+        if num_teams < 2:
+            print("The minimum number of teams is 2, try again.")
+            continue
+        else:
+            break
+    return num_teams
+
+
+def get_num_games(num_teams):
+    while True:
+        num_games = int(
+            input("Enter the number of games played by each team: "))
+        if num_games < num_teams - 1:
+            print("Invalid number of games. Each team plays each other at least once in the regular season, try again.")
+            continue
+        else:
+            break
+    return num_games
+
+
+def get_team_name(team_num):
+    while True:
+        team_name = input(f"Enter the name for team #{team_num}: ")
+        if len(team_name.split()) > 2:
+            print("Team names may have at most 2 words, try again.")
+            continue
+        elif len(team_name) < 2:
+            print("Team names must have at least 2 characters, try again.")
+        else:
+            break
+    return team_name
+
+
+def get_team_score(teams, games):
+    scores = []
+    for team in teams:
+        while True:
+            team_score = int(
+                input(f"Enter the number of wins Team {team} had: "))
+            if team_score < 0:
+                print("The minimum number of wins is 0, try again.")
+                continue
+            elif team_score > games:
+                print(f"The maximum number of wins is {games}, try again.")
+            else:
+                scores.append(team_score)
+                break
+    return scores
+
+
 def main():
-    pass
+
+    teams = []
+    num_teams = get_num_teams()
+
+    for i in range(1, num_teams + 1):
+        name = get_team_name(i)
+        teams.append(name)
+
+    num_games = get_num_games(num_teams)
+    scores = get_team_score(teams, num_games)
+    teams_and_scores = list(zip(teams, scores))
+    start_low = sorted(teams_and_scores, key=lambda x: x[1])
+    start_high = sorted(teams_and_scores, key=lambda x: x[1], reverse=True)
+
+    print("Generating the games to be played in the first round of the tournament...")
+    for i in range(int(len(teams)/2)):
+        print(f"Home: {start_low[i][0]} VS Away: {start_high[i][0]}")
+
+
+main()
